789ff4f12d4485242a411f714ff125ce1bf494a2
BANGAKE <lgdemetrius@gmail.com>
2025-03-18 03:31:13 +0100

    Suppresion de About et Blog pour corriger les erreurs

--- src/app/About/page.tsx
+++
@@ -1,9 +0,0 @@
-// Exemple pour Blog
-/*export default function BlogPage() {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold">Blog</h1>
-        <p className="text-gray-600 mt-2">Bienvenue</p>
-      </div>
-    );
-  }*/
